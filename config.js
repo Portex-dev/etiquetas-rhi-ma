0871355d284a9b5f1bbfbe0d249a6554030faa15
Lucas <lucasportex.dev@gmail.com>
2026-09-14 21:48:16 -0300

    Configurar endereço público do aplicativo

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ window.APP_CONFIG = {
   supabaseUrl: "https://ihvcllgybungaivwmars.supabase.co",
   supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlodmNsbGd5YnVuZ2FpdndtYXJzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk0Mjc3ODEsImV4cCI6MjEwNTAwMzc4MX0.J57NQ9_e0WgPL4bciHJp1ktkg2acWUTAXzvhLkaBjsE",
   turnstileSiteKey: "",
-  publicSiteUrl: window.location.origin + window.location.pathname
+  publicSiteUrl: "https://portex-dev.github.io/etiquetas-rhi-ma/"
 };
