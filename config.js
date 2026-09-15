98950d56189325b8fe72b8cd96a0d5fda2fb67fb
Lucas <lucasportex.dev@gmail.com>
2026-09-14 22:16:44 -0300

    Ativar proteção Turnstile no formulário

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
   supabaseUrl: "https://ihvcllgybungaivwmars.supabase.co",
   supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlodmNsbGd5YnVuZ2FpdndtYXJzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk0Mjc3ODEsImV4cCI6MjEwNTAwMzc4MX0.J57NQ9_e0WgPL4bciHJp1ktkg2acWUTAXzvhLkaBjsE",
-  turnstileSiteKey: "",
+  turnstileSiteKey: "0x4AAAAAAE031yZpQb3Ijnz2",
   publicSiteUrl: "https://portex-dev.github.io/etiquetas-rhi-ma/"
 };
